7147a67d36d65cb94f27a4dec363bb4b0f786ca6
LucasAlves2803 <alveshugo2906@gmail.com>
2024-03-21 12:16:41 -0300

    ok

--- source/script/main.py
+++ source/script/main.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import os
 import glob
+
+# caminho para ler os arquivos
+folder_path = 'source\\raw_data\\'
+
+# lista todos os arquivos de excel
+excel_files = glob.glob(os.path.join(folder_path , '*.xlsx'))
+
+print(excel_files)
