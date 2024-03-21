a7a38acb57e455f50e18bf32b0da2cfe3ba29423
LucasAlves2803 <alveshugo2906@gmail.com>
2024-03-21 15:29:26 -0300

    ok

--- source/script/main.py
+++ source/script/main.py
@@ -9,7 +9,7 @@ folder_path = 'source\\raw_data\\'
 excel_files = glob.glob(os.path.join(folder_path , '*.xlsx'))
 
 # aqui printamos o arquivo, podemos ver que é um array completo
-print(excel_files)
+# print(excel_files)
 
 # aqui verificamos se or arquivos estão dentro do excel_files, caso não o arquivo não será encontrado
 if not excel_files:
@@ -26,14 +26,38 @@ else:
             # print("oi",df_temp)
             # pegando os nomes dos arquivos
             file_name = os.path.basename(files)
-            print(file_name)
+            # print(file_name)
+
 
             if "brasil" in file_name.lower():
                 df_temp["location"] =  "br"
             elif "france" in file_name.lower():
                 df_temp["location"] =  "fr"
             elif "italia" in file_name.lower():
-                df_temp["location"] in file_name.lower()
+                df_temp["location"] = "it"
+
+            # print(df_temp)    
+
+
+            # criamos uma nova coluna chamada campaign
+            df_temp['campaign'] = df_temp['utm_link'].str.extract(r'utm_campaign=(.*)')
+
+            # guarda dados tratados dentro de uma dataframe
+            new_dataframe.append(df_temp)
+
+            print(new_dataframe)
 
         except:
             print("sem arquivo")    
+
+if new_dataframe:
+ result = pd.concat(new_dataframe, ignore_index=True)
+    
+#caminho de saída
+output_file = os.path.join('source', 'ready', 'final.xlsx')
+#configurou o motor de escrita
+writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+# leva os dados do resultado a serem escritos no motor de excel configurado
+result.to_excel(writer, index=False)
+#salva o arquivo de excel
+writer._save()
