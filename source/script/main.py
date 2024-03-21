af0502a56a97144cdcbf9222c9334f25e19d44ff
LucasAlves2803 <alveshugo2906@gmail.com>
2024-03-21 13:41:29 -0300

    ok

--- source/script/main.py
+++ source/script/main.py
@@ -8,4 +8,12 @@ folder_path = 'source\\raw_data\\'
 # lista todos os arquivos de excel
 excel_files = glob.glob(os.path.join(folder_path , '*.xlsx'))
 
+# aqui printamos o arquivo, podemos ver que é um array completo
 print(excel_files)
+
+# aqui verificamos se or arquivos estão dentro do excel_files, caso não o arquivo não será encontrado
+if not excel_files:
+    print("Não arquivo não encontrado")
+else:
+# Aqui começamos o código, o dataframe irá receber nossas modificações e salva-lás
+    new_dataframe = []    
